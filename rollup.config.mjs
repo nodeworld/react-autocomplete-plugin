5b528b442a5cf96f727c43c16cd7c560ba8d3dac
nodeworld <ordiflow@gmail.com>
2024-09-01 20:52:22 +0530

    Remove storybook and update rollup config.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,39 +1,40 @@
-import commonjs from '@rollup/plugin-commonjs';
-import resolve from '@rollup/plugin-node-resolve';
-import typescript from '@rollup/plugin-typescript';
-import dts from 'rollup-plugin-dts';
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
 import terser from '@rollup/plugin-terser';
-
-import packageJson from './package.json' assert { type: 'json' };
-
+import pkg from './package.json' assert { type: 'json' };
+import dts from 'rollup-plugin-dts';
 export default [
-    {
-        input: 'src/index.ts',
-        output: [
-            {
-                file: packageJson.main,
-                format: 'cjs',
-                sourcemap: false
-            },
-            {
-                file: packageJson.module,
-                format: 'esm',
-                sourcemap: false
-            }
-        ],
-        plugins: [
-            resolve(),
-            commonjs(),
-            typescript({ tsconfig: './tsconfig.json', exclude: ['**/*.test.tsx', '**/*.test.ts', '**/*.stories.ts'] }),
-            postcss({ extensions: ['.css'], inject: true, extract: false }),
-            terser()
-        ]
-    },
-    {
-        input: 'dist/esm/types/index.d.ts',
-        output: [{ file: 'dist/index.d.ts', format: 'esm' }],
-        plugins: [dts()],
-        external: [/\.css$/],
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: pkg.main,
+        format: "cjs",
+        sourcemap: true,
       },
-]
+      {
+        file: pkg.module,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonjs(),
+      typescript({ tsconfig: "./tsconfig.json" }),
+      postcss({ extensions: ['.css'], inject: true, extract: false }),
+      terser()
+    ],
+    external: ["react", "react-dom"]
+  },
+  {
+    input: 'dist/esm/index.d.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
+    plugins: [dts()],
+    external: [/\.css$/],
+  },
+];
